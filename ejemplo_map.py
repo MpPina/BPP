ce5c5b088c97fd3f0c4b267463fefebc3b75f0c3
MpPina <ppinavaras@gmail.com>
2021-12-30 19:51:20 +0100

    mejora eficiencia: funcion map

--- ejemplo_map.py
+++ ejemplo_map.py
@@ -1,7 +1,7 @@
 numeros=[2,4,5,6,8,10]
 
-resultado=[]
-for i in numeros:
-    resultado.append(i**2)
+def elevar(n):
+    return n**2
 
-print(resultado)
+resultado2=list(map(elevar,numeros))  
+print(resultado2)
